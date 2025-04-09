8f00e77242027ef8120bf2f258b2e0e6631a4369
Elodieisj <elodie.jannin@sfr.fr>
2025-04-09 12:00:26 +0200

    Add files via upload

---
+++ lvl1-ex3.js
@@ -0,0 +1,8 @@
+function askNumber() {
+    alert ("Please enter two numbers")
+    let numbera = prompt ("a")
+    let numberb = prompt ("b")
+    let sum = parseFloat(numbera)+parseFloat(numberb)
+console.log (sum)
+}
+askNumber () 
